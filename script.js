78b2ca3ab3bbd409a0f8a51503cec8362bde4ede
brijrajsinh parmar <brijrajparmar27@gmail.com>
2022-09-16 15:54:05 +0530

    iteration v1

---
+++ script.js
@@ -0,0 +1,11 @@
+setInterval(() => {
+    setTimeout(() => {
+        document.getElementsByClassName("artdeco-dropdown__trigger artdeco-dropdown__trigger--placement-bottom ember-view mn-connection-card__dropdown-trigger artdeco-button--tertiary artdeco-button--muted artdeco-button--circle p1")[0].click()
+    }, 250)
+    setTimeout(() => {
+        document.getElementsByClassName("display-flex align-items-center t-14 t-black--light t-normal")[2].click()
+    }, 250)
+    setTimeout(() => {
+        document.getElementsByClassName("artdeco-modal__confirm-dialog-btn artdeco-button artdeco-button--2 artdeco-button--primary ember-view")[0].click()
+    }, 250)
+}, 1000)
